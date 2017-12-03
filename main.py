830d39244a25476717a801b844eff9b295c45ed3
Tim <tsbkwd@mst.edu>
2017-12-03 17:13:56 -0600

    Initial commit. Added stub classes and files

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+from bot import *
+from world import *
+
+
+
+if __name__ == '__main__':
+    temp = bot()
+    print(temp.memory)
+    print("HERE")
---
+++ world.py
@@ -0,0 +1,8 @@
+class world:
+    def __init__(self):
+        self.map = self.getMap()
+
+
+    def getMap(self):
+        return ""
+
